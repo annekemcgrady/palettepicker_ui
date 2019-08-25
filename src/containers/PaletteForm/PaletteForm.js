a2080c37588ab41825657bd88c9d8cf7760c0efa
Taylor Jordan <taylornoel418@icloud.com>
2019-08-25 15:12:41 -0600

    Add methods to update state to palette form

--- src/containers/PaletteForm/PaletteForm.js
+++ src/containers/PaletteForm/PaletteForm.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { postPalette } from '../../utilz/apiCalls';
+import { addPalette, hasErrored } from '../../actions'
 import './PaletteForm.scss';
 
 class PaletteForm extends React.Component {
     constructor() {
         super();
         this.state = {
-            name: '',
-            color_one: '',
-            color_two: '',
-            color_three: '',
-            color_four: '',
-            color_five: ''
+            name: ''
         }
     }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }  
     
+    handleSubmit = e => {
+        e.preventDefault();
+        const newPalette = {...this.state}
+        postPalette(newPalette)
+        .then(palette => this.props.addPalette(palette))
+        .catch(error => this.props.hasErrored(error))
+        
+        this.clearInputs()
+    }
+
+    clearInputs = () => {
+        this.setState({ name: '' })
+    }
+
     render() {
         return (
-            <form>
-                <input type='text' name='name' value={this.state.name} placeholder='Name Your Palette!'/>
+            <form className='save-palette-form'>
+                <input type='text' name='name' value={this.state.name} placeholder='Name Your Palette!' onChange={this.handleChange}/>
+                <button onClick={e => this.handleSubmit(e)}>Save</button>
             </form>
         )
     }
 }
 
-export default PaletteForm;
+const mapStateToProps = state => ({
+    palettes: state.palettes,
+    error: state.error
+})
+
+const mapDispatchToProps = dispatch => ({
+    addPalette: palette => dispatch(addPalette(palette)),
+    hasErrored: error => dispatch(hasErrored(error))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PaletteForm);
