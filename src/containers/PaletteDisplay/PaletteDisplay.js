5cc7373b1c3cad15eb807beeba77afeb26d7273e
Anneke McGrady <annekemcgrady@gmail.com>
2019-08-25 14:54:42 -0600

    Create initial palette display container and palette tile

--- src/containers/PaletteDisplay/PaletteDisplay.js
+++ src/containers/PaletteDisplay/PaletteDisplay.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import PaletteTile from '../../components/PaletteTile/PaletteTile.js'
 import './PaletteDisplay.scss';
 
-const PaletteDisplay = () => {
+export const PaletteDisplay = ({ currentColors }) => {
+
+    const displaySwatches = currentColors.map(color => {
+        return (
+        <PaletteTile
+            key={Date.now()}
+            isLocked={false}
+            hexCode={color.hexCode}
+         />
+        )
+    })
+
     return (
-        <section>
+        <section className='palette-display'>
+
+            {displaySwatches }
 
         </section>
     )
-}
+    
+};
 
-export default PaletteDisplay;
+export const mapStateToProps = state => ({
+    currentColors: state.colors
+});
+
+export default connect(mapStateToProps)(PaletteDisplay);
