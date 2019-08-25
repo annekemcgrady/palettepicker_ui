e4f00e6e7e993ad26b9ce15f4aa84b0200155852
Taylor Jordan <taylornoel418@icloud.com>
2019-08-25 14:31:26 -0600

    Add fetch calls to apiCalls file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Welcome to Palette Picker!</h1>
+    </main>
+  )
 }
 
 export default App;
--- src/utilz/apiCalls.js
+++ src/utilz/apiCalls.js
@@ -1,6 +1,6 @@
 export const fetchProjects = async () => {
     try {
-        const url = `http://localhost:3000/api/v1/projects`
+        const url = `http://localhost:3001/api/v1/projects`
         const response = await fetch(url);
         if(!response.ok) {
             throw new Error('Projects failed to fetch');
@@ -14,7 +14,7 @@ export const fetchProjects = async () => {
 
 export const fetchPalettes = async () => {
     try {
-        const url = `http://localhost:3000/api/v1/palettes`
+        const url = `http://localhost:3001/api/v1/palettes`
         const response = await fetch(url)
         if(!response.ok) {
             throw new Error('Palettes failed to fetch')
@@ -26,6 +26,94 @@ export const fetchPalettes = async () => {
     }
 }
 
-export const postProject = async () => {
-    
+export const postProject = async (project) => {
+    try {
+        const url = `http://localhost:3001/api/v1/projects`
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(project)
+        }
+
+        const response = await fetch(url, options) 
+        const addedProject = response.json();
+
+        return addedProject;
+    } catch ({ message }) {
+        throw new Error(message)
+    }
 }
+
+export const postPalette = async (palette) => {
+    try {
+        const url = `http://localhost:3001/api/v1/palettes`
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(palette)
+        }
+        const response = await fetch(url, options)
+        const addedPalette = response.json()
+
+        return addedPalette;
+    } catch ({ message }) {
+        throw new Error(message)
+    }
+}
+
+export const patchProject = async (project) => {
+    try {
+        const url = `http://localhost:3001/api/v1/projects/${project.id}`
+        const options = {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(project)
+        }
+
+        const response = await fetch(url, options)
+        const editedProject = response.json()
+
+        return editedProject;
+    } catch ({ message }) {
+        throw new Error(message)
+    }
+}
+
+export const patchPalette = async (palette) => {
+    try {
+        const url = `http://localhost:3001/api/v1/palettes/${palette.id}`
+        const options = {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(palette)
+        }
+
+        const response = await fetch(url, options)
+        const editedPalette = response.json();
+
+        return editedPalette;
+    } catch ({ message }) {
+        throw new Error(message)
+    }
+}
+
+export const deleteProject = async (project) => {
+    try {
+        const url = `http://localhost:3001/api/v1/projects/${project.id}`
+        const response = await fetch(url)
+
+        return response;
+    } catch ({ message }) {
+        throw new Error(message)
+    }
+}
+
+export const deletePalette = async (palette) => {
+    try {
+        const url = `http://localhost:3001/api/v1/palettes/${palette.id}`
+
+    } catch ({ message }) {
+        throw new Error(message)
+    }
+}
+
