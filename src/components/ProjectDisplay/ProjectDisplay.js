4943ae2763fd4d5316c61ee63a4af803d2bf7135
Taylor Jordan <taylornoel418@icloud.com>
2019-08-26 07:37:00 -0600

    Add fetch and actions to get projects into project display component

--- src/components/ProjectDisplay/ProjectDisplay.js
+++ src/components/ProjectDisplay/ProjectDisplay.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ProjectForm from '../../containers/ProjectForm/ProjectForm';
+import ProjectTile from '../ProjectTile/ProjectTile';
+import { fetchProjects } from '../../utilz/apiCalls';
+import { getProjects, hasErrored, loadComplete } from '../../actions';
+import { connect } from 'react-redux';
+import { mapStateToProps, mapDispatchToProps } from '../../containers/PaletteDisplay/PaletteDisplay';
 
-const ProjectDisplay = () => {
+const ProjectDisplay = (props) => {
+    const projects = fetchProjects()
+    .then(projects => props.getProjects(projects))
+    .catch(error => props.hasErrored(error))
     return (
         <section className='project-display'>
             <ProjectForm />
@@ -9,4 +17,16 @@ const ProjectDisplay = () => {
     )
 }
 
-export default ProjectDisplay;
+const mapStateToProps = state => ({
+    projects: state.projects,
+    error: state.error,
+    isLoading: state.loading
+})
+
+const mapDispatchToProps = dispatch => ({
+    getProjects: projects => dispatch(getProjects(projects)),
+    hasErrored: error => dispatch(hasErrored(error)),
+    loadComplete: () => dispatch(loadComplete())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectDisplay);
