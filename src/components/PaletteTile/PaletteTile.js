6131519320ef4dcd8629f1d247da6fde08702c4e
Anneke McGrady <annekemcgrady@gmail.com>
2019-08-26 05:21:25 -0600

    Fix number of colors in colors array and generate random hexcodes

--- src/components/PaletteTile/PaletteTile.js
+++ src/components/PaletteTile/PaletteTile.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './PaletteTile.scss'
 
 const PaletteTile = ({ isLocked, hexCode }) => {
+
+    var divStyle = {
+        color: 'white',
+        backgroundColor: `#${hexCode}`,
+        height: '300px',
+        width: '200px',
+        border: '1px solid black'
+    };
+    
     return (
-        <article className='palette-tile'>
-            <p>{hexCode}</p>
+        <article className='palette-tile' style={divStyle}>
+            <p>#{hexCode}</p>
 
 
         </article>
--- src/containers/PaletteDisplay/PaletteDisplay.js
+++ src/containers/PaletteDisplay/PaletteDisplay.js
@@ -1,56 +1,55 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import PaletteTile from '../../components/PaletteTile/PaletteTile.js'
-import './PaletteDisplay.scss';
-import ColorScheme from 'color-scheme'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getColors } from "../../actions";
+import PaletteTile from "../../components/PaletteTile/PaletteTile.js";
+import "./PaletteDisplay.scss";
+import ColorScheme from "color-scheme";
 
 export class PaletteDisplay extends Component {
+  generateColors = () => {
+    let randomHue = Math.floor(Math.random() * 500 + 1);
+    let scheme = new ColorScheme();
+    scheme
+      .from_hue(randomHue)
+      .scheme("contrast")
+      .variation("hard");
 
-    state = {
-        colors: []
-    }
+    var colors = scheme.colors().splice(0, 5);
+    // this.setState({colors})
+    this.props.setCurrentColors(colors);
+  };
 
-generateColors =() => {
-let scheme = new ColorScheme;
-    scheme.from_hue(21)         // Start the scheme 
-      .scheme('contrast')     // Use the 'triade' scheme, that is, colors
-                            // selected from 3 points equidistant around
-                            // the color wheel.
-      .variation('hard');   
+  render = () => {
+    console.log(this.props.currentColors);
 
-var colors = scheme.colors();
-console.log(colors)
-this.setState(colors)
-this.props.setCurrentColors(this.state.colors)
-}
-
-render = () => {
     const displaySwatches = this.props.currentColors.map(color => {
-        return (
-        <PaletteTile
-            key={Date.now()}
-            isLocked={false}
-            hexCode={color.hexCode}
-        />
-        )
-    })
+      return <PaletteTile key={Date.now()} isLocked={false} hexCode={color} />;
+    });
 
     return (
-        <section className='palette-display'>
-            {displaySwatches }
-        <button type='button' className='generate-new-palette' onClick={this.generateColors}>Generate New Palette</button>
-        </section>
-    )
+      <section className="palette-display">
+        {displaySwatches}
+        <button
+          type="button"
+          className="generate-new-palette"
+          onClick={this.generateColors}
+        >
+          Generate New Palette
+        </button>
+      </section>
+    );
+  };
 }
-    
-};
 
 export const mapStateToProps = state => ({
-    currentColors: state.colors
+  currentColors: state.colors
 });
 
 export const mapDispatchToProps = dispatch => ({
-    setCurrentColors: newColors => dispatch(addColors(newColors))
-})
+  setCurrentColors: newColors => dispatch(getColors(newColors))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(PaletteDisplay);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PaletteDisplay);
