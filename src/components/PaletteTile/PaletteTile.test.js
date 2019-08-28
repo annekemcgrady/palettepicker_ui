6c24b823be6f51c3714214e11de24fd6d7db383a
Anneke McGrady <annekemcgrady@gmail.com>
2019-08-28 10:55:02 -0600

    Add snapshot to paletteTile and MDTP and MSTP tests to PaletteDisplay

--- src/components/PaletteTile/PaletteTile.test.js
+++ src/components/PaletteTile/PaletteTile.test.js
@@ -5,11 +5,21 @@ import { shallow } from 'enzyme';
 
 describe('PaletteTile', () => {
   let wrapper;
-  let instance;
   let props;
 
-  it('should match the snapshot', ()=>{
+  beforeEach(() => {
+    props = {
+      isLocked:false,
+      hexCode: '#123ABC',
+      id: '#123ABC',
+      lockColor: jest.fn()
+    };
+  
+    wrapper = shallow(<PaletteTile{...props}/>);
+  })
 
+  it('should match the snapshot', ()=> {
+    expect(wrapper).toMatchSnapshot();
   })
 
 
--- src/containers/PaletteDisplay/PaletteDisplay.test.js
+++ src/containers/PaletteDisplay/PaletteDisplay.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PaletteDisplay, mapStateToProps, mapDispatchToProps} from './PaletteDisplay';
-import { getProjects, getPalettes, hasErrored, loadComplete, removePalette } from '../../actions';
+import { getColors } from '../../actions';
 import { shallow } from 'enzyme';
 
 
@@ -10,11 +10,77 @@ describe('PaletteDisplay', () => {
   let props;
 
 
-  it.skip('should match the snapshot', ()=>{
+  beforeEach(() => {
+  props = {
+    colors: [{hexCode:'#123457', isLocked: false}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}],
+    setColors: jest.fn()
+  };
+
+  wrapper = shallow(<PaletteDisplay{...props}/>);
+  instance = wrapper.instance();
+
+})
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
 
   })
 
+ describe('generateColors', () => {
+  it.skip('should call generateColors when Generate New Colors button clicked', ()=> {
+      
+      instance.generateColors = jest.fn();
+      wrapper.find('.generate-new-palette-button').simulate('click')
+      expect(instance.generateColors).toHaveBeenCalled()
+    })
 
+  it('should dispatch a setColors action with an array of five hexcodes', ()=> {
 
+  })
+
+})
+
+ describe('lockColor', ()=> {
+
+  it('should call setColors with a new array of colors, ()=> {
+    let expected = [{hexCode:'#123457', isLocked: true}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}, {hexCode:'#123456', isLocked: false}]
+    instance.lockColor('#123457')
+    const mockDispatch = jest.fn();
+    expect(mockDispatch).toHaveBeenCalledWith(expected)
+  })
+
+  it('should dispatch a setColors action with an array of five hexcodes', ()=> {
+
+  })
+
+  })
+
+  describe('mapStateToProps', ()=> {
+
+    it('should return colors from store', () => {
+      const mockState = {
+          colors: [{color: '123456'}, {color: 'ffffff'}]
+      }
+
+      const expected = {
+          colors: [{color: '123456'}, {color: 'ffffff'}]
+      }
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+  });
+
+  })
+
+  describe('mapDispatchToProps', ()=> {
+
+    it('should call dispatch with a setColors action', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = getColors([{hexCode: '#123ABC'}]);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setColors([{hexCode: '#123ABC'}])
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  })
 
 })
